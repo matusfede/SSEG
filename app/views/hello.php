341c558936bbc4fa149db25adbe5cfc8295e1bbd
seach-sam <search.sama@gmail.comm>
2014-08-31 00:35:21 -0600

    primer commit

---
+++ app/views/hello.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Flat UI - Free Bootstrap Framework and Themes</title>
+    <meta name="description" content="Flat UI Kit Free is a Twitter Bootstrap Framework design and Theme, this responsive framework includes a PSD and HTML version."/>
+
+    <meta name="viewport" content="width=1000, initial-scale=1.0, maximum-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Loading Flat UI -->
+    <link href="css/flat-ui.css" rel="stylesheet">
+    <link href="css/demo.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <!-- Barra de menus -->
+  	<div class="col-xs-14">
+        <nav class="navbar navbar-inverse navbar-embossed" role="navigation">
+            <div class="navbar-header">
+              	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
+                	<span class="sr-only">Toggle navigation</span>
+              	</button>
+            </div>
+            <div class="collapse navbar-collapse" id="navbar-collapse-01">
+              	<ul class="nav navbar-nav navbar-left">           
+                	<li><a href="#fakelink">SSEG<span class="navbar-unread">1</span></a></li>
+                	<li><a href="#fakelink">Quienes somos</a></li>
+                	<li><a href="#fakelink">Empresas</a></li>
+                	<li><a href="#fakelink">Ofertas</a></li>
+                	<li><a href="#fakelink">Contactenos</a></li>
+                	<li><a href="#fakelink">Crea tu cuenta</a></li>
+               	</ul>
+            </div><!-- /.navbar-collapse -->
+        </nav><!-- /navbar -->
+    </div>
+
+    <div class="container">
+      <div class="demo-headline">
+        <h1 class="demo-logo">
+          <div class="logo"></div>
+          Flat UI
+          <small>Free User Interface Kit</small>
+        </h1>
+      </div> <!-- /demo-headline -->
+
+      <div class="row">
+        <div class="col-xs-9">
+          <div class="demo-browser">
+            <div class="demo-browser-side">
+              <div class="demo-browser-author"></div>
+              <div class="demo-browser-action">
+                <a class="btn btn-danger btn-lg btn-block" href="http://twitter.com/monstercritic" target="_blank">
+                  Follow
+                </a>
+              </div>
+              <h5>@monstercritic</h5>
+              <h6>
+                Tourist. Designer. NYC
+                <a href="http://shmidt.in" target="_blank">shmidt.in</a>
+              </h6>
+            </div>
+            <div class="demo-browser-content">
+              <img src="images/demo/browser-pic-1.jpg" alt="" />
+              <img src="images/demo/browser-pic-2.jpg" alt="" />
+              <img src="images/demo/browser-pic-3.jpg" alt="" />
+              <img src="images/demo/browser-pic-4.jpg" alt="" />
+              <img src="images/demo/browser-pic-5.jpg" alt="" />
+              <img src="images/demo/browser-pic-6.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-3">
+          <div class="demo-download">
+            <img src="images/demo/html-icon.png" src="Free PSD" />
+          </div>
+
+          <a href="https://github.com/designmodo/Flat-UI/archive/master.zip" class="btn btn-primary btn-lg btn-block">Download</a>
+
+          <p class="demo-download-text">Your likes, shares and comments make us happy!</p>
+        </div>
+      </div> <!-- /download area -->
+
+    </div> <!-- /container -->
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-7">
+            <h3 class="footer-title">Subscribe</h3>
+            <p>Do you like this freebie? Want to get more stuff like this?<br/>
+              Subscribe to designmodo news and updates to stay tuned on great designs.<br/>
+              Go to: <a href="http://designmodo.com/flat-free" target="_blank">designmodo.com/flat-free</a>
+            </p>
+
+            <p class="pvl">
+              <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://designmodo.com/flat-free/" data-text="Flat UI Free - PSD&amp;amp;HTML User Interface Kit" data-via="designmodo">Tweet</a>
+							<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+              <iframe src="http://ghbtns.com/github-btn.html?user=designmodo&repo=Flat-UI&type=watch&count=true" height="20" width="107" frameborder="0" scrolling="0" style="width:105px; height: 20px;" allowTransparency="true"></iframe>
+              <iframe src="http://ghbtns.com/github-btn.html?user=designmodo&repo=Flat-UI&type=fork&count=true" height="20" width="107" frameborder="0" scrolling="0" style="width:105px; height: 20px;" allowTransparency="true"></iframe>
+              <iframe src="http://ghbtns.com/github-btn.html?user=designmodo&type=follow&count=true" height="20" width="195" frameborder="0" scrolling="0" style="width:195px; height: 20px;" allowTransparency="true"></iframe>
+            </p>
+
+            <a class="footer-brand" href="http://designmodo.com" target="_blank">
+              <img src="images/footer/logo.png" alt="Designmodo.com" />
+            </a>
+          </div> <!-- /col-xs-7 -->
+
+          <div class="col-xs-5">
+            <div class="footer-banner">
+              <h3 class="footer-title">Get Flat UI Pro</h3>
+              <ul>
+                <li>Tons of Basic and Custom UI Elements</li>
+                <li>A Lot of Useful Samples</li>
+                <li>More Vector Icons and Glyphs</li>
+                <li>Pro Color Swatches</li>
+                <li>Bootstrap Based HTML/CSS/JS Layout</li>
+              </ul>
+              Go to: <a href="http://designmodo.com/flat" target="_blank">designmodo.com/flat</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+    <!-- Load JS here for greater good =============================-->
+    <script src="js/jquery-1.8.3.min.js"></script>
+    <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
+    <script src="js/jquery.ui.touch-punch.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-select.js"></script>
+    <script src="js/bootstrap-switch.js"></script>
+    <script src="js/flatui-checkbox.js"></script>
+    <script src="js/flatui-radio.js"></script>
+    <script src="js/jquery.tagsinput.js"></script>
+    <script src="js/jquery.placeholder.js"></script>
+    <script src="js/jquery.stacktable.js"></script>
+    <script src="http://vjs.zencdn.net/4.3/video.js"></script>
+    <script src="js/application.js"></script>
+  </body>
+</html>
