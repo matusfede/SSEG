d7d83305dfdac30b361d925af6b8d2645f8eeead
seach-sam <search.sama@gmail.comm>
2015-02-28 02:49:42 -0600

    creacion de la pagina de inicio

--- app/views/sitio/home.blade.php
+++ app/views/sitio/home.blade.php
@@ -11,13 +11,12 @@
 	<link rel="stylesheet" href="coolkitten/css/normalize.css" type="text/css" media="screen">
 	<link rel="stylesheet" href="coolkitten/css/grid.css" type="text/css" media="screen">
 	<link rel="stylesheet" href="coolkitten/css/style.css" type="text/css" media="screen">
-	<!-- <link rel="stylesheet" href="css/style.min.css" type="text/css" media="screen"> -->
-	<!--[if IE]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<link rel="stylesheet" href="css/sseg.css" type="text/css" media="screen">
 </head>
 
 <body>
 
-	<div class="menu">
+	<div class="menu" id="top_menu">
 		<div class="container clearfix">
 
 			<div id="logo" class="grid_3">
@@ -36,22 +35,30 @@
 		</div>
 	</div>
 
+	
+
 
 	<div class="slide" id="slide1" data-slide="1" data-stellar-background-ratio="0.5">
 		<div class="container clearfix">
 
-			<div id="content" class="grid_7">
-				<h1>Top Slide</h1>
-				<h2>Get ready to make magic!</h2>
-				<p>Remember that this is a BETA version. This is my first framework so if you see any issue please <a href="mailto:jalxob@gmail.com">let me know it</a>. </p>
-				<p>Don't forget to follow me!</p>
-				<p><a href="https://twitter.com/Jalxob" target="_blank"><img src="coolkitten/images/twitter.png"></a> <a href="http://dribbble.com/jalxob" target="_blank"><img src="coolkitten/images/dribbble.png"></a></p>
-			</div>
-			<div id="decorative" class="grid_5 omega">
-				<img src="coolkitten/images/decorative.png">
+			<div style="display: table; margin: 0 auto;">
+				<img src="image/logo.svg">
 			</div>
 
 		</div>
+
+		<div id="bottom_menu">
+		<div class="container clearfix">
+			<div id="nav" class="grid_12">
+				<ul class="navigation">
+					<li data-slide="1"><span>Quienes Somos</span></li>
+					<li data-slide="2"><span>Empresa</span></li>
+					<li data-slide="3"><span>Contactenos</span></li>
+					<li data-slide="4"><span>Ingrese</span></li>
+				</ul>
+			</div>
+		</div>
+	</div>
 	</div>
 
 
@@ -108,11 +115,11 @@
 		</div>
 	</div>
 
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script type="text/javascript" src="coolkitten/js/jquery.stellar.min.js"></script>
 	<script type="text/javascript" src="coolkitten/js/waypoints.min.js"></script>
 	<script type="text/javascript" src="coolkitten/js/jquery.easing.1.3.js"></script>
 	<script type="text/javascript" src="coolkitten/js/scripts.js"></script>
-	<!-- <script type="text/javascript" src="js/scripts.min.js"></script> -->
+	<script type="text/javascript" src="js/sseg.js"></script>
 </body>
 </html>
