1ae922386b42f76a745685d05baa95870be2e3a4
seach-sam <search.sama@gmail.comm>
2015-03-20 15:39:54 -0600

    cambio de colores de secion

--- system/app/views/sitio/home.blade.php
+++ system/app/views/sitio/home.blade.php
@@ -65,10 +65,11 @@
 
 
 		<div class="slide" id="slide2" data-slide="1">
+			<div class="rivete clearfix"> <h1>¿Quienes Somos?</h1> </div>
 			<div class="container clearfix">
 
 				<div id="content" class="grid_12">
-					<h1>Parallax Scrolling</h1>
+					<h1>Quines somos?</h1>
 					<h2>What you've seen its called parallax scrolling</h2>
 				</div>
 
@@ -81,22 +82,9 @@
 			<div class="container clearfix">
 
 				<div id="content" class="grid_12">
-					<h1>Grid</h1>
+					<h1>Empresa</h1>
 					<h2>See how the grid changes when you resize your screen</h2>
 				</div>
-
-				<div id="test" class="grid_1">1</div> <div id="test" class="grid_11 omega">11</div>
-				<div id="test" class="grid_2">2</div> <div id="test" class="grid_10 omega">10</div>
-				<div id="test" class="grid_3">3</div> <div id="test" class="grid_9 omega">9</div>
-				<div id="test" class="grid_4">4</div> <div id="test" class="grid_8 omega">8</div>
-				<div id="test" class="grid_5">5</div> <div id="test" class="grid_7 omega">7</div>
-				<div id="test" class="grid_6">6</div> <div id="test" class="grid_6 omega">6</div>
-				<div id="test" class="grid_7">7</div> <div id="test" class="grid_5 omega">5</div>
-				<div id="test" class="grid_8">8</div> <div id="test" class="grid_4 omega">4</div>
-				<div id="test" class="grid_9">9</div> <div id="test" class="grid_3 omega">3</div>
-				<div id="test" class="grid_10">10</div> <div id="test" class="grid_2 omega">2</div>
-				<div id="test" class="grid_11">11</div> <div id="test" class="grid_1 omega">1</div>
-				<div id="test" class="grid_12">12</div>
 
 			</div>
 		</div>
