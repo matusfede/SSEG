e2da24b2b91625f1a36182e369dce843a2b32998
search-sam <samaviles@gmail.com>
2016-02-29 16:13:11 -0600

    Nuevo comienzo de actividad

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,32 +2,30 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Routes File
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
+| Here is where you will register all of the routes in an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
 */
 
-// Ruta de Inicio
 Route::get('/', function () {
-    //return view('welcome');
-    return view('sitio.inicio');
+    return view('welcome');
 });
 
-// Rutas de Autentificacion...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
 
-// Rutas de Registro...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
-
-// Rutas del Sitio...
-//Route::any('/home', 'Sitio\SitioController@Inicio');
-
-// Rutas del Sistema
-Route::any('/home', 'Dashboard\SistemaController@inicio');
+Route::group(['middleware' => ['web']], function () {
+    //
+});
--- public/js/sitio.js
+++
@@ -1,9 +0,0 @@
-// archivo JavaScript de sitio
-
-$(document).ready(function(){
-  // Funcion para obtener el Alto(Height)
-  console.log("El alto del navegador es " + $( window ).height() + "px. (Height)");
-  console.log($('.modal-content').css( "height" ));
-  var position = (($( window ).height()) / 2) - (254 / 2);
-  console.log("La posicion del modal es " + position + "px. (Top)");
-});
